bdce5301a45903434c08f40888ebf1fe1661102b
League <league@Mac-mini-4.local>
2019-08-17 11:26:29 -0700

    fortune cookie update
    added button and frame

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,4 +1,17 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class FortuneCookie {
+	public void showButton() {
+	     JFrame frame= new JFrame();
+	 frame.setVisible(true);
+	 JButton button = new JButton();
+	 frame.add(button);
+	 frame.pack();
 
+
+	 
+	 
+	    }
+	
 }
--- src/FortuneCookieRunner.java
+++ src/FortuneCookieRunner.java
@@ -2,8 +2,7 @@
 public class FortuneCookieRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new FortuneCookie().showButton();
 	}
 
 }
