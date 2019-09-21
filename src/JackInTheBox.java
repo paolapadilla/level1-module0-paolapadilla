b9939063b1bc6356a6d0c039eacde7b74f3b7789
ComputerCodingMaster <hellothereobiwan1@gmail.com>
2019-09-21 11:25:35 -0700

    started jack in the box

--- src/JackInTheBox.java
+++ src/JackInTheBox.java
@@ -1,55 +1,39 @@
-import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.print.DocFlavor.URL;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class JackInTheBox {
+public class JackInTheBox implements ActionListener {
+       int count = 0;    
+       JButton button = new JButton();
+	public static void main(String[] args) {
+		new JackInTheBox().showButton();
+		
+		
 
-	public JackInTheBox() {
-		System.out.println("Button clicked");
 	}
-
-	private void showPicture(String fileName) {
-		try {
-			JLabel imageLabel = createLabelImage(fileName);
-			JFrame frame = new JFrame();
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.add(imageLabel);
-			frame.setVisible(true);
-			frame.pack();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+public void showButton() {
+	JFrame frame = new JFrame();
+		JPanel panel = new JPanel();	
+		frame.add(panel);
+		frame.add(button);
+		frame.pack();
+		button.setLabel("Surprise");
+		frame.setVisible(true);
+		button.addActionListener(this);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	if(button.equals(e.getSource())) {
+		count++;
+		System.out.println("button clicked");
 	}
-
-	private JLabel createLabelImage(String fileName) {
-		try {
-			java.net.URL imageURL = getClass().getResource(fileName);
-			if (imageURL == null) {
-				System.err.println("Could not find image " + fileName);
-				return new JLabel();
-			} else {
-				Icon icon = new ImageIcon(imageURL);
-				JLabel imageLabel = new JLabel(icon);
-				return imageLabel;
-			}
-		} catch (Exception e) {
-			System.err.println("Could not find image " + fileName);
-			return new JLabel();
-		}
+	if(count ==5) {
+		System.out.println("pressed 5 times");
 	}
+}
 
-	private void playSound(String soundFile) {
-		try {
-			AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
-			sound.play();
-		} catch (Exception e) {
-			e.printStackTrace();
 
-		}
-	}
 }
