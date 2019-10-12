08e53e3bc136b239fc8ec918c571462862c3ce1b
paolapadilla <paolapadilla061@gmail.com>
2019-10-12 10:57:39 -0700

    i finished Jack In The Box.
    i am not going to be attending workshop next week because i will be in a program

--- src/JackInTheBox.java
+++ src/JackInTheBox.java
@@ -1,39 +1,88 @@
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class JackInTheBox implements ActionListener {
-       int count = 0;    
-       JButton button = new JButton();
+	int count = 0;
+	JButton button = new JButton();
+
 	public static void main(String[] args) {
 		new JackInTheBox().showButton();
-		
-		
 
 	}
-public void showButton() {
-	JFrame frame = new JFrame();
-		JPanel panel = new JPanel();	
+
+	public void showButton() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
 		frame.add(panel);
 		frame.add(button);
 		frame.pack();
 		button.setLabel("Surprise");
 		frame.setVisible(true);
 		button.addActionListener(this);
-}
-@Override
-public void actionPerformed(ActionEvent e) {
-	if(button.equals(e.getSource())) {
-		count++;
-		System.out.println("button clicked");
 	}
-	if(count ==5) {
-		System.out.println("pressed 5 times");
-	}
-}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (button.equals(e.getSource())) {
+			count++;
+			System.out.println("button clicked");
+		}
+		if (count == 5) {
+			showPicture("jackinthebox.png");
+			playSound("homer-woohoo.wav");
+
+			System.out.println("pressed 5 times");
+		}
+
+	}
+
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+	}
+
+	private void playSound(String soundFile) {
+		try {
+			AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+			sound.play();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
