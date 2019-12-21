efb2860e1b0a6b8c1dc746f6cb703ec53dbb48a5
paolapadilla <league@WTS-MM-7.local>
2019-12-21 11:25:57 -0800

    i am a step away from finishing

--- src/sound_effects_machine/SoundEffectsMachine.java
+++ src/sound_effects_machine/SoundEffectsMachine.java
@@ -11,18 +11,12 @@ public class SoundEffectsMachine implements ActionListener {
 	private static JButton button1;
 	private static JButton button2;
 	private static JButton button3;
-	private JButton button2;
-	private JButton button3;
 
-	public static void main(String[] args) { new SoundEffectsMachine().showButton();
-
-
-
-
-
-
+	public static void main(String[] args) {
+		new SoundEffectsMachine().showButton();
 
 	}
+
 	public void showButton() {
 		System.out.println("Button clicked");
 		JFrame frame = new JFrame();
@@ -35,11 +29,11 @@ public class SoundEffectsMachine implements ActionListener {
 		panel.add(button1);
 		panel.add(button2);
 		panel.add(button3);
-		button1.addActionListener();
+		button1.addActionListener(this);
 		button1.setText("Alien Spaceship UFO");
-		button2.addActionListener();
+		button2.addActionListener(this);
 		button2.setText("Cartoon phone Ring");
-		button3.addActionListener();
+		button3.addActionListener(this);
 		button3.setText("Cartoon Birds 2");
 
 	}
@@ -48,8 +42,30 @@ public class SoundEffectsMachine implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if(button1.equals(e.getSource())){
+			playSound("Alien Spaceship UFO.wav");
+
+		}
+			if(button2.equals(e.getSource())){
+			playSound("Cartoon-birds-2_daniel-simion.wav");
+
+		}
+		if(button3.equals(e.getSource())){
+			playSound("cartoon-telephone_daniel_simion.wav");
+
+	}
+
+
+
 
 
 	}
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
+
+
+
 }
  
