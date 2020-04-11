391c6d87909c6652ceac0226992aba94413926b6
root <root@2b13e17f41da>
2020-04-11 18:33:09 +0000

    i restarted duck.

---
+++ src/Duck.java
@@ -0,0 +1,21 @@
+
+public class Duck {
+
+private static final String String = null;
+
+String favoriteFood;
+int numberOfFriends;
+Duck(String favorite,int numberOfFriends){
+	this.favoriteFood = favoriteFood;
+	this.numberOfFriends = numberOfFriends;
+}
+public void quack(){
+	System.out.println("Quack!");
+}
+
+{
+		
+		
+	}
+
+}
