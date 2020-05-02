7ede591b1c4c030a7c2d896adfd7c97fef869cc0
root <root@4af67c8bdc14>
2020-05-02 18:34:16 +0000

    Working on binary Convertor

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -3,12 +3,12 @@ import javax.swing.JOptionPane;
 public class BinaryConverter {
 
 	public static void main(String[] args) {
-		String convert(String input) {
-			if(input.length()!=8){
-			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s,silly!!!");
-			return"-";			
-			}
+		
+	}
+	public void convert(String input) {
+		if(input.length()!=8){
+ 	JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s,silly!!!");
+		return "-";			
 		}
 	}
-
 }
